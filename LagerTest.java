afbfec77c21b059b98c8a3b983af5e0c4d770dab
sebastienst <sebastien.steinbach@laposte.net>
2016-12-10 15:53:32 +0100

    Update LagerTest.java

--- LagerTest.java
+++ LagerTest.java
@@ -1,5 +1,3 @@
-
-
 import static org.junit.Assert.*;
 import org.junit.After;
 import org.junit.Before;
@@ -14,6 +12,10 @@ import org.junit.Test;
 public class LagerTest
 {
     private Lager lager;
+    
+    private final static String LA_AN = "artikel werden dazu gemacht";
+    private final static String LA_EN = "nicht genuegent artikel";
+
     /**
      * Default constructor for test class LagerTest
      */
@@ -42,6 +44,14 @@ public class LagerTest
     {
     }
     
+    
+    @Test(expected = RuntimeException.class)
+    public void testKein_Lager_Name(){
+        Lager lager2;
+        lager2 = new Lager("", 3);
+        
+    }
+    
     @Test(expected = RuntimeException.class)
     public void testKonstructorIntNull(){
         Lager lager2;
@@ -52,5 +62,23 @@ public class LagerTest
     public void testKonstructorIntNegative(){
         Lager lager2;
         lager2 = new Lager("nico",-100);
+    }
+    
+   
+     @Test
+    public void testArtikelAnlegen(){
+        Lager.anzArtikel(2);
+        assertEquals(LA_AN,
+            new Artikel(7548, "Chips", 0, 2.89),
+            anzArtikel++);
+    
+    }
+    
+    @Test
+    public void testArtikelEntfernen(){
+        Lager.anzArtikel(3);
+        assertEquals(LA_EN,
+            anzArtikel--);
+    
     }
 }
